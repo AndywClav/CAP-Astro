d1b6c75bbac2e7e647dbc784a1050b483cf5f659
AndywClav <andresclavijo0822@gmail.com>
2026-07-09 00:47:37 -0300

    :sparkles: Add service implementation stub

---
+++ srv/library.ts
@@ -0,0 +1,12 @@
+import cds from "@sap/cds";
+import { Books, Chapters, Authors } from "#cds-models/LibraryService";
+
+export class LibraryService extends cds.ApplicationService {
+  init() {
+    this.before(["CREATE", "UPDATE"], Books, async (req) => {
+      console.log("Before CREATE/UPDATE Books", req.data);
+    });
+
+    return super.init();
+  }
+}
